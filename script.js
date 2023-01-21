920a7b81fcff6631f0e98d08843ffd2022138fa1
JadiDapa <119063058+JadiDapa@users.noreply.github.com>
2023-01-21 08:34:45 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let buttons = document.querySelector('.buttons') ;
+let btn = document.querySelectorAll('span') ;
+let value = document.getElementById('value');
+
+for(i = 0; 1< btn.length; i++) {
+    btn[i].addEventListener('click', function(){
+
+        if(this.innerHTML == "=") {
+            value.innerHTML = eval(value.innerHTML);
+        } else {
+            if(this.innerHTML == "clear") {
+                value.innerHTML = "";
+            } else {
+                value.innerHTML += this.innerHTML;
+            }
+        }
+    })
+}
