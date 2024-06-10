5e331758d3ccd428087a8ba31560f66257a3229c
JadiDapa <daffafalthaf25@gmail.com>
2024-06-10 21:15:11 +0700

    feat : project list updates

--- src/section/Project.jsx
+++ src/section/Project.jsx
@@ -1,83 +1,79 @@
-import alirsyad from "../assets/image/alirsyad.png";
-import afdanews from "../assets/image/afdanews.png";
-import ytclone from "../assets/image/ytclone.png";
-import nike from "../assets/image/nike.png";
-import weather from "../assets/image/weather.png";
-import cart from "../assets/image/cart.png";
-import { GoLinkExternal } from "react-icons/go";
-import { useEffect } from "react";
-import ScrollReveal from "scrollreveal";
+import alirsyad from '../assets/image/alirsyad.png';
+import PLNHPUP6 from '../assets/image/PLNHPUP6.png';
+import ynsect from '../assets/image/ynsect.png';
+import nike from '../assets/image/nike.png';
+import portfolio from '../assets/image/portfolio.png';
+import spotify from '../assets/image/spotify.png';
+import { BiLogoGithub } from 'react-icons/bi';
+import { useEffect } from 'react';
+import ScrollReveal from 'scrollreveal';
 
 const Project = () => {
   const projects = [
     {
-      link: "https://al-irsyad-jadidapa.vercel.app/",
+      link: 'https://github.com/JadiDapa/al-irsyad',
       image: alirsyad,
-      title: "AL - IRYSAD  |  Mosque Profile",
+      title: 'AL - IRYSAD  |  Mosque Profile'
     },
     {
-      link: "https://afdanews.000webhostapp.com/index.php/login",
-      image: afdanews,
-      title: "Afda News  |  Article & News Web",
+      link: 'https://github.com/JadiDapa/distribution-tracking-app-fe',
+      image: PLNHPUP6,
+      title: 'Distribution & Inventory Management'
     },
     {
-      link: "https://dapa-tube.vercel.app/",
-      image: ytclone,
-      title: "Youtube Clone  |  API Management",
+      link: 'https://github.com/zildjianvitoo/ynsect-fe',
+      image: ynsect,
+      title: 'Ynsect  |  Food Waste Management'
     },
     {
-      link: "https://nike-react-tailwind.vercel.app/",
+      link: 'https://github.com/JadiDapa/spotify-clone',
+      image: spotify,
+      title: 'Music Library App'
+    },
+    {
+      link: 'https://github.com/JadiDapa/JadiDapa.github.io',
+      image: portfolio,
+      title: 'Personal Portfolio'
+    },
+    {
+      link: 'https://nike-react-tailwind.vercel.app/',
       image: nike,
-      title: "Nike Landing Page  |  Learning Project",
-    },
-    {
-      link: "https://weather-app-react-jadidapa.vercel.app/",
-      image: weather,
-      title: "Weather Forecast  |  Learning Project",
-    },
-    {
-      link: "#",
-      image: cart,
-      title: "E-Commerence  |  Learning Project",
-    },
+      title: 'Nike Landing Page  |  Learning Project'
+    }
   ];
   useEffect(() => {
     const sr = ScrollReveal({
-      distance: "40px",
+      distance: '40px',
       duration: 2000,
-      delay: 200,
+      delay: 200
     });
 
-    sr.reveal(".mypro", { origin: "top" });
-    sr.reveal(".projects", { origin: "bottom" });
+    sr.reveal('.mypro', { origin: 'top' });
+    sr.reveal('.projects', { origin: 'bottom' });
   }, []);
 
   return (
-    <section id="project" className="overflow-hidden px-4 lg:px-28 py-5">
-      <div className="container mx-auto py-10 flex items-center flex-col">
-        <h2 className="mypro font-bold text-5xl md:text-6xl">
+    <section id="project" className="px-4 py-5 overflow-hidden lg:px-28">
+      <div className="container flex flex-col items-center py-10 mx-auto">
+        <h2 className="text-5xl font-bold mypro md:text-6xl">
           MY <span className="custom-text-gradient">PROJECTS</span>
         </h2>
-        <div className="projects grid md:p-0 p-8 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-6 mt-12">
+        <div className="grid grid-cols-1 gap-6 p-8 mt-12 projects md:p-0 lg:grid-cols-3 md:grid-cols-2">
           {projects.map((project, index) => (
             <a
               href={project.link}
               target="_blank"
               rel="noopener noreferrer"
               key={index}
-              className="group duration-300 p-3 bg-dark2 hover:border-primary border border-transparent rounded-lg relative hover:bg-transparent"
+              className="relative p-3 duration-300 border border-transparent rounded-lg group bg-dark2 hover:border-primary hover:bg-transparent "
             >
-              <div className="border border-transparent duration-300 group-hover:border-primary rounded-lg overflow-hidden relative w-full">
-                <div className="absolute group-hover:translate-y-0 translate-y-full bg-secondary/60 duration-300 w-full h-full flex flex-col justify-center items-center gap-2">
-                  <GoLinkExternal className="text-4xl" />
-                  <a className="text-xl ">See the Project</a>
+              <div className="relative w-full overflow-hidden duration-300 border border-transparent rounded-lg group-hover:border-primary">
+                <div className="absolute flex flex-col items-center justify-center w-full h-full gap-2 duration-300 translate-y-full group-hover:translate-y-0 bg-secondary/60">
+                  <BiLogoGithub className="text-4xl" />
+                  <a className="text-xl ">See The Code</a>
                 </div>
 
-                <img
-                  src={project.image}
-                  alt=""
-                  className="w-full duration-300"
-                />
+                <img src={project.image} alt="" className="w-full duration-300 min-h-[250px]" />
               </div>
               <p className="mt-2 text-lg">{project.title}</p>
             </a>
